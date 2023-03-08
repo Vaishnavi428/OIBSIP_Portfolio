ea54d3a13efd2ff5197486018ed286969402a4ad
VAISHNAVI GADHAVE <vgadhave411@gmail.com>
2023-03-09 00:14:55 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import React from "react";
+import './App.css';
+import Home from "./routes/Home";
+import About from "./routes/About";
+import Project from "./routes/Project";
+import Contact from "./routes/Contact";
+import { Route, Routes } from "react-router-dom";
+
 function App() {
   return (
-    <div className="App">
-      Subscribe ZainKeepsCode
-    </div>
+    <>
+    <Routes>
+      <Route path="/" element={<Home /> } />
+      <Route path="/project" element={<Project /> } />
+      <Route path="/about" element={<About /> } />
+      <Route path="/contact" element={<Contact /> } />
+    </Routes>
+    </>
   );
 }
 
